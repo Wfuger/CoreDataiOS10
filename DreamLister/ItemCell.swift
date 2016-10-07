862a33070e70d530f1e64a1cbd83d34d368a4645
Wfuger <wfuger@gmail.com>
2016-10-07 16:15:07 -0500

    Table view cell update method, fetch results controller

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Will Fuger on 10/7/16.
+//  Copyright © 2016 BoogieSquad. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        titleLabel.text = item.title
+        priceLabel.text = "$\(item.price)"
+        detailLabel.text = item.details
+        
+    }
+
+}
